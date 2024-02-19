4678be832dee548cb583d3b26961e4dc5e42b510
YounBrassebin <118263730+YounBrassebin@users.noreply.github.com>
2024-02-19 11:44:00 +0100

    Ajout des 3 vues

--- app/src/main/java/com/example/andoidgsb/MainActivity.java
+++ app/src/main/java/com/example/andoidgsb/MainActivity.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button buttonAjout = (Button) findViewById(R.id.buttonAjoutEchan);
+    Button buttonListe = (Button) findViewById(R.id.buttonListeEchan);
+
+    Button buttonMaj = (Button) findViewById(R.id.buttonMajEchan);
 
     @Override
     // OnCreatz
@@ -22,5 +25,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, AjoutEchantillon.class));
             }
         });
+
+        buttonListe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ListeEchantillon.class));
+            }
+        });
+
+        buttonMaj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MajEchantillon.class));
+            }
+        });
+
     }
 }
