f90581ae2acbf154cb4977c80411c7c7bd26f5ff
YounBrassebin <118263730+YounBrassebin@users.noreply.github.com>
2024-02-19 11:22:44 +0100

    Rajoute titre et 3 boutton

--- app/src/main/java/com/example/andoidgsb/MainActivity.java
+++ app/src/main/java/com/example/andoidgsb/MainActivity.java
@@ -2,14 +2,25 @@ package com.example.andoidgsb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonAjout = (Button) findViewById(R.id.buttonAjoutEchan);
 
     @Override
     // OnCreatz
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAjout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AjoutEchantillon.class));
+            }
+        });
     }
 }
